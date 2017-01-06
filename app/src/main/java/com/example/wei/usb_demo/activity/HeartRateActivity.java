45be3938eec5cda228d4b67873103efb16b5b413
WeiLitest <wei.li@dnurse.cn>
2017-01-06 17:03:37 +0800

    画折线图

--- app/src/main/java/com/example/wei/usb_demo/activity/HeartRateActivity.java
+++ app/src/main/java/com/example/wei/usb_demo/activity/HeartRateActivity.java
@@ -9,11 +9,14 @@ import com.example.wei.usb_demo.activity.base.BaseActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.view.LineChartView;
@@ -27,9 +30,11 @@ public class HeartRateActivity extends BaseActivity {
     private ArrayList<AxisValue> axisXValues = new ArrayList<AxisValue>();
     private LineChartData lineChartData;
     private Axis axisY, axisX;
+    private Timer timer;
+    private int x = 5;
 
     final static int MAX_X_VALUE = 1000;
-    final static int MAX_Y_VALUE = 40;
+    final static int MAX_Y_VALUE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,35 +46,36 @@ public class HeartRateActivity extends BaseActivity {
         int height = layoutParams.height;
         int width = layoutParams.width;
 
-        int right = MAX_Y_VALUE * width / height;
+        final int right = MAX_Y_VALUE * width / height;
 
         _heartRateLineView.setViewportCalculationEnabled(false);
         _heartRateLineView.setMaximumViewport(new Viewport(0.0f, MAX_Y_VALUE, MAX_X_VALUE, 0.0f));
         _heartRateLineView.setCurrentViewport(new Viewport(0.0f, MAX_Y_VALUE, right, 0.0f));
         _heartRateLineView.setZoomEnabled(false);
+        _heartRateLineView.setInteractive(false);
 
         for (int index = 0; index < MAX_Y_VALUE; index += 2) {
-            axisYValues.add(new AxisValue(index).setLabel(""+index));
+            axisYValues.add(new AxisValue(index).setLabel("" + index));
         }
         for (int index = 0; index < MAX_X_VALUE; index += 2) {
-            axisXValues.add(new AxisValue(index).setLabel(""));
+            axisXValues.add(new AxisValue(index));
         }
 
-//        Line spo2Line = new Line().setColor(Color.GREEN);  //折线的颜色（橙色）
-//        List<Line> spo2Lines = new ArrayList<Line>();
-//        spo2Line.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
-//        spo2Line.setStrokeWidth(1);
-//        spo2Line.setPointRadius(0);
-//        spo2Line.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
-//        spo2Line.setHasPoints(false);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
-//        spo2Lines.add(spo2Line);
+        Line spo2Line = new Line().setColor(Color.GREEN);  //折线的颜色（橙色）
+        List<Line> spo2Lines = new ArrayList<Line>();
+        spo2Line.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
+        spo2Line.setStrokeWidth(1);
+        spo2Line.setPointRadius(0);
+        spo2Line.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
+        spo2Line.setHasPoints(false);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
+        spo2Lines.add(spo2Line);
 
         lineChartData = new LineChartData();
-//        lineChartData.setLines(spo2Lines);
+        lineChartData.setLines(spo2Lines);
         //坐标轴
         axisX = new Axis(); //X轴
         axisX.setHasTiltedLabels(false);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
-        axisX.setTextColor(Color.RED);  //设置字体颜色
+//        axisX.setTextColor(Color.RED);  //设置字体颜色
         axisX.setTextSize(7);//设置字体大小
         axisX.setValues(axisXValues);  //填充X轴的坐标名称
         axisX.setHasLines(true); //x 轴分割线
@@ -84,5 +90,37 @@ public class HeartRateActivity extends BaseActivity {
         lineChartData.setAxisYLeft(axisY);  //Y轴设置在左边
 
         _heartRateLineView.setLineChartData(lineChartData);
+        _heartRateLineView.addLineToPoint(new PointValue(0, 7));
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                if (x > 988) {
+                    this.cancel();
+                    _heartRateLineView.setInteractive(true);
+                    return;
+                }
+                _heartRateLineView.addLineToPoint(new PointValue(x, 7));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 1, 10));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 2, 6));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 3, 18));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 4, 7));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 5, 9));
+                _heartRateLineView.addLineToPoint(new PointValue(x + 6, 8));
+
+                _heartRateLineView.setCurrentViewport(new Viewport(x, MAX_Y_VALUE, x + right, 0));
+
+                x += 10;
+            }
+        }, 200, 1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        timer.cancel();
     }
 }
