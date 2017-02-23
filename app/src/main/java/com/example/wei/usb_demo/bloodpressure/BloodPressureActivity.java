d840cd46c31f2485153022fb331de799c7800dfe
甄强 <qiang.zhen@dnurse.cn>
2017-02-23 18:25:45 +0800

    测量倒计时控件

--- app/src/main/java/com/example/wei/usb_demo/bloodpressure/BloodPressureActivity.java
+++ app/src/main/java/com/example/wei/usb_demo/bloodpressure/BloodPressureActivity.java
@@ -2,17 +2,22 @@ package com.example.wei.usb_demo.bloodpressure;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.wei.pl2303_test.R;
 import com.example.wei.usb_demo.activity.base.BaseActivity;
+import com.example.wei.usb_demo.data.views.CircleLoadingProgressView;
+
 
 public class BloodPressureActivity extends BaseActivity {
     private static final String TAG = "BloodPressureActivity";
 
     private String deviceKey = "";
     private boolean usbDeviceDiscerned;
+    private CircleLoadingProgressView progressView;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +31,50 @@ public class BloodPressureActivity extends BaseActivity {
 
         Button btn2 = (Button) findViewById(R.id.real_time_btn);
         btn2.setOnClickListener(btnOnClickListener);
+        progressView = (CircleLoadingProgressView) findViewById(R.id.pregress);
+        progressView.start();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         toolbar.setTitle("测量血压");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 5;
+                while (i != -1) {
+                    try {
+                        Thread.sleep(1000);
+                        final int m = i;
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressView.setProgress(m);
+                            }
+                        });
+                        i--;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressView.setVisibility(View.GONE);
+                        progressView.stop();
+                    }
+                });
+            }
+        }).start();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        progressView.setVisibility(View.GONE);
+        progressView.stop();
+    }
 
     private View.OnClickListener btnOnClickListener = new View.OnClickListener() {
         public void onClick(View v) {
